cadf508ac00c8806514acad321993379a6839718
DawnteaStudio <dawnteabro@naver.com>
2021-09-16 23:04:49 +0900

    My first Crolling
    네이버 기사 크롤링 첫 성공!!!

--- scrapping/02week.py
+++ scrapping/02week.py
@@ -3,12 +3,17 @@ from selenium import webdriver
 
 driver = webdriver.Chrome('./chromedriver')
 
-url = "https://search.naver.com/search.naver?where=news&sm=tab_jum&query=추석"
+url = "https://search.naver.com/search.naver?where=news&sm=tab_jum&query=헝다"
 
 driver.get(url)
 req = driver.page_source
 soup = BeautifulSoup(req, 'html.parser')
+articles = soup.select('#main_pack > section > div > div.group_news > ul > li')
 
-print("hi")
+for article in articles:
+    title = article.select_one('div.news_wrap.api_ani_send > div > a').text
+    print(title)
+    # title = article.select_one('div > div > a').text 이렇게 경로를 잘 못 찾으면 프린트가 안된다
 
 driver.quit()
+
