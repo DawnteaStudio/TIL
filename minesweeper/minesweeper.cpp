a9b895de92c8c99b0ee4b03bba064fae77f28b44
DawnteaStudio <dawnteabro@naver.com>
2022-05-05 15:32:19 +0900

    [DOCS] Make minesweeper
    I make minesweeper's basic code

---
+++ minesweeper/minesweeper.cpp
@@ -0,0 +1,32 @@
+#include "minesweeper.h"
+
+void minesweeper( string input, string output )
+{
+	// Implement this
+}
+
+void initBoard( char board[][SIZE], string file )
+{
+	// Implement this
+}
+
+void displayBoard( ostream& out, char board[][SIZE], bool touchedBoard[][SIZE], bool gameOver )
+{
+	// Implement this
+}
+
+void initTouched( bool touchedBoard[][SIZE] )
+{
+	// Implement this
+}
+
+void touchBoard( bool board[][SIZE], int x, int y )
+{
+	// Implement this
+}
+
+int main(){
+    minesweeper(/*Your command file path*/, /*Your output file path*/);
+	
+	return 0;
+}
