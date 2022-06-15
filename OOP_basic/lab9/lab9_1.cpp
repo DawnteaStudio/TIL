05f1218f3d352b0353f92e4f45a64aefb37c75d6
DawnteaStudio <dawnteabro@naver.com>
2022-06-16 01:35:48 +0900

    Update lab9_1.cpp

--- OOP_basic/lab9/lab9_1.cpp
+++ OOP_basic/lab9/lab9_1.cpp
@@ -1,65 +1,34 @@
+#include <iostream>
 #include <vector>
-#include <random>
 #include <iomanip>
 using namespace std;
+using Matrix = vector<vector<int>>;
 
-vector<vector<int> > input(int row, int col) {
-	vector<vector<int> > matrix(row, vector<int>(col));
-	for (vector<int>& v : matrix) {
-		for (int& elem : v)
-			elem = rand() % 19 - 9;
-	}
-	return matrix;
-}
 
-void print_vec(vector<vector<int> > v) {
-	for (vector<int>& v1 : v) {
-		for (int& elem : v1)
-			cout << setw(6) << elem;
-		cout << endl;
-	}
+// 배열의 경우 주소값(시작주소, 끝주소)을 전달
+void print(int* begin, int* end) {
+	// for (int* curr = begin; curr < end; curr++) //수업시간에 배운 주소값을 기준으로 for문 작성
+	// cout << setw(4) << *curr;
+	
+	//  while문 구현 부분 - 수업시간에 배운 주소값을 기준으로 while문 작성
+		int* curr = begin;
+		while (curr < end) {
+			cout << setw(4) << *curr;
+			curr++;
+		}
 	cout << endl;
 }
 
-vector<vector<int> > multiply(vector<vector<int> >A, vector<vector<int> >B) {
-	int row1 = A.size(), col1 = A[0].size();
-	int row2 = B.size(), col2 = B[0].size();
-	vector<vector<int> > matrix(row1,vector<int>(col2));
-
-	for (int i = 0; i < row1; i++) {
-		for (int j = 0; j < col2; j++){
-			matrix[i][j] = 0;
-			for (int k = 0; k < col1; k++) 
-				matrix[i][j] += A[i][k] * B[k][j];
-		}
-	}
-	return matrix;
-}
-
+// (+, -) for pointer: 주소값을 증가/감소 (다음 변수 위치)
 int main() {
-	cout << "A의 행, 열의 크기를 입력해주세요 : ";
-	int row1, row2, col1, col2;
-	cin >> row1 >> col1;
-	cout << "B의 행, 열의 크기를 입력해주세요 : ";
-	cin >> row2 >> col2;
+	int list[3] = { 10, 20, 30 };
+	cout << list << '\t' << list[0] << endl;
+	cout << list + 1 << '\t' << list[1] << endl;
+	cout << list + 2 << '\t' << list[2] << endl;
 
-	vector<vector<int> > A = input(row1, col1);
+	int *begin = list;
+	int* end = list + 3;
+	print(begin, end);
 
-	vector<vector<int> > B = input(row2, col2);
-
-	cout << "A 행렬 :\n";
-	print_vec(A);
-	cout << "B 행렬 :\n";
-	print_vec(B);
-
-	if (col1 != row2) {
-		cout << "두 행렬을 곱할 수 없습니다.";
-		return 0;
-	}
-
-	vector<vector<int> > AB = multiply(A, B);
-	cout << "AB 곱행렬 :\n";
-	print_vec(AB);
-    cout << endl;
 	return 0;
 }
