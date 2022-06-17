19721b0f4ad5fb5d648f19f5194c28bcf99eec68
DawnteaStudio <dawnteabro@naver.com>
2022-06-18 00:24:38 +0900

    Create lab6-6_6.cpp

---
+++ OOP_basic/lab5-6/lab6-6_6.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	char key;
+	cout << "Enter a key (p or q): ";
+	cin >> key;
+	switch (key) {
+	case 'p':
+    case 'P':
+        cout << "Pasue"<<endl;
+        break;
+    case 'q':
+    case 'Q':
+        cout << "Quit"<<endl;
+        break;
+
+	default:
+		cout << "Default"<<endl;
+	}
+	return 0;
+}
