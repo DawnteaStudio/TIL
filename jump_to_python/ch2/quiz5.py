fb5b461605a06bad98846ec3b8aec016eade0a14
DawnteaStudio <dawnteabro@naver.com>
2022-07-20 23:19:47 +0900

    update quizs
    +question

--- jump_to_python/ch2/quiz5.py
+++ jump_to_python/ch2/quiz5.py
@@ -1,4 +1,5 @@
 #quiz5
+#문자열 : 를 #로 바꾸기
 a = "a:b:c:d"
 b = a.replace(":","#")
 print(b)
--- jump_to_python/ch2/quiz7.py
+++ jump_to_python/ch2/quiz7.py
@@ -1,4 +1,5 @@
 #quiz7
+#리스트를 문자열로 만들기
 a = ['life', 'is', 'too', 'short']
 result = ' '.join(a)
 print(result)
