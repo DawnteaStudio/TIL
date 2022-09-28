ccfad5605f68bd02d565d59a7bba162d8f68e312
DawnteaStudio <dawnteabro@naver.com>
2022-09-28 23:44:07 +0900

    Update final_2.py

--- jump_to_python/final 20 quiz/final_2.py
+++ jump_to_python/final 20 quiz/final_2.py
@@ -1,41 +1,5 @@
-# # quiz1
-# result = 0
-# i = 1
-# while i <= 1000:
-#     if i % 3 == 0:
-#         result += i
-#     i += 1
-# print(result)
+#딕셔너리의 a에서 'C'라는 key에 해당하는 value를 출력하고자 한다.
+#하지만 딕셔너리상에는 'C'가 없다고 가정하고 오류 대신 70을 얻도록 작성하라.
 
-# # quiz2
-
-# star = 1
-# while star < 6:
-#     print("*"*star)
-#     star += 1
-
-# # quiz3
-# for i in range(1,101):
-#     print(i)
-
-# quiz4
-score = [70,60,55,75,95,90,80,80,85,100]
-sum = 0
-for i in score:
-    sum += i
-print("Mean Value is" , sum/len(score))
-pp = sum/len(score)
-print("Mean Value is %f" % pp)
-
-# quiz5 basic
-numbers = [1,2,3,4,5]
-result = []
-for i in numbers:
-    if i % 2 != 0:
-        result.append(i*2)
-print(result)
-
-# quiz5 list comprehension
-numbers = [1,2,3,4,5]
-result = [i*2 for i in numbers if i%2 != 0]
-print(result)
+a = {'A':90,'B':80}
+print(a.get('C',70))
