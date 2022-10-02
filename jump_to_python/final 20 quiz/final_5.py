28cba7380b14a4404de16974bb6e854fc3fe19d9
DawnteaStudio <dawnteabro@naver.com>
2022-10-02 22:52:46 +0900

    Update final_5.py

--- jump_to_python/final 20 quiz/final_5.py
+++ jump_to_python/final 20 quiz/final_5.py
@@ -1,41 +1,20 @@
-# # quiz1
-# result = 0
-# i = 1
-# while i <= 1000:
-#     if i % 3 == 0:
-#         result += i
-#     i += 1
-# print(result)
+#첫항은 0이고 n번째 항까지의 피보나치 수열을 출력하는 함수를 작성하라
 
-# # quiz2
+num1 = 0
+num2 = 1
+goal = int(input("n값 입력 : "))
 
-# star = 1
-# while star < 6:
-#     print("*"*star)
-#     star += 1
+for i in range(goal):
 
-# # quiz3
-# for i in range(1,101):
-#     print(i)
-
-# quiz4
-score = [70,60,55,75,95,90,80,80,85,100]
-sum = 0
-for i in score:
-    sum += i
-print("Mean Value is" , sum/len(score))
-pp = sum/len(score)
-print("Mean Value is %f" % pp)
-
-# quiz5 basic
-numbers = [1,2,3,4,5]
-result = []
-for i in numbers:
-    if i % 2 != 0:
-        result.append(i*2)
-print(result)
-
-# quiz5 list comprehension
-numbers = [1,2,3,4,5]
-result = [i*2 for i in numbers if i%2 != 0]
-print(result)
+    if goal == 1:
+        print(num1)
+    elif goal == 2:
+        print(num1, num2)
+        break
+    else:
+        if i%2 == 0:
+            print(num1, end = ' ')
+            num1 += num2
+        elif i%2 == 1:
+            print(num2, end = ' ')
+            num2 += num1
