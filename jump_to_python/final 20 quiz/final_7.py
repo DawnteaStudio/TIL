476f94b21ef453854b31af6c471ca598ad21ae1b
DawnteaStudio <dawnteabro@naver.com>
2022-10-04 22:50:37 +0900

    Update final_7.py

--- jump_to_python/final 20 quiz/final_7.py
+++ jump_to_python/final 20 quiz/final_7.py
@@ -1,41 +1,6 @@
-# # quiz1
-# result = 0
-# i = 1
-# while i <= 1000:
-#     if i % 3 == 0:
-#         result += i
-#     i += 1
-# print(result)
+#사용자로부터 2~9를 입력받고 해당 숫자의 구단 출력
 
-# # quiz2
+num = int(input("2 ~ 9 입력 : "))
 
-# star = 1
-# while star < 6:
-#     print("*"*star)
-#     star += 1
-
-# # quiz3
-# for i in range(1,101):
-#     print(i)
-
-# quiz4
-score = [70,60,55,75,95,90,80,80,85,100]
-sum = 0
-for i in score:
-    sum += i
-print("Mean Value is" , sum/len(score))
-pp = sum/len(score)
-print("Mean Value is %f" % pp)
-
-# quiz5 basic
-numbers = [1,2,3,4,5]
-result = []
-for i in numbers:
-    if i % 2 != 0:
-        result.append(i*2)
-print(result)
-
-# quiz5 list comprehension
-numbers = [1,2,3,4,5]
-result = [i*2 for i in numbers if i%2 != 0]
-print(result)
+for i in range(1,10):
+    print(num * i, end = " ")
