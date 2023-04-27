ca3ffb83206a8b17997b4b5667b0737bb6523a64
DawnteaStudio <dawnteabro@naver.com>
2023-04-28 00:54:35 +0900

    ?

--- C-CPP_workspace/khu_OOP/OOP_Quiz/lab12/fixedtext.cpp
+++
@@ -1,5 +0,0 @@
-#include "fixedtext.h"
-
-	FixedText::FixedText() : Text::Text("FIXED") {}
-	void FixedText::append(string _extra) {
-    } //override 키워드 사용한 append()함수 구현, main함수 참고하여 출력화면처럼 되도록 구현
