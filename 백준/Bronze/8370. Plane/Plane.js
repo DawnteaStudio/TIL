e83240c210822aaa7ffb27463f763aa3a5fece1a
DawnteaStudio <90132183+DawnteaStudio@users.noreply.github.com>
2023-05-04 17:32:12 +0900

    [Bronze V] Title: Plane, Time: 120 ms, Memory: 9576 KB -BaekjoonHub

---
+++ 백준/Bronze/8370. Plane/Plane.js
@@ -0,0 +1,11 @@
+//basic setting
+var fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+
+//split setting (what you want) (Must be "trim()")
+var input = fs.readFileSync(filePath).toString().trim().split(' ').map(Number);
+
+//cording here
+let [n1, k1, n2, k2] = input;
+
+console.log(n1*k1+n2*k2);
