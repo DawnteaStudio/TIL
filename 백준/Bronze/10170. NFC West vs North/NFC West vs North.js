a4b58f0aeb8f2fe7917c7b6a362ef80e2a57ce0a
DawnteaStudio <90132183+DawnteaStudio@users.noreply.github.com>
2023-05-04 17:55:34 +0900

    [Bronze V] Title: NFC West vs North, Time: 136 ms, Memory: 9304 KB -BaekjoonHub

---
+++ 백준/Bronze/10170. NFC West vs North/NFC West vs North.js
@@ -0,0 +1,23 @@
+//basic setting
+var fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+
+//split setting (what you want) (Must be "trim()")
+var input = fs.readFileSync(filePath).toString().trim();
+
+//cording here
+let str= `NFC West       W   L  T
+-----------------------
+Seattle        13  3  0
+San Francisco  12  4  0
+Arizona        10  6  0
+St. Louis      7   9  0
+
+NFC North      W   L  T
+-----------------------
+Green Bay      8   7  1
+Chicago        8   8  0
+Detroit        7   9  0
+Minnesota      5  10  1`
+
+console.log(str);
