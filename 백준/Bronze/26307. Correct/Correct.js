bc36f0f0993cf020d0aeb1b7aff0d411f2ee6349
DawnteaStudio <90132183+DawnteaStudio@users.noreply.github.com>
2023-05-05 03:18:29 +0900

    [Bronze V] Title: Correct, Time: 136 ms, Memory: 9352 KB -BaekjoonHub

---
+++ 백준/Bronze/26307. Correct/Correct.js
@@ -0,0 +1,11 @@
+//basic setting
+var fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+
+//split setting (what you want) (Must be "trim()")
+// input 유형 별로 split(공백 or 줄바꿈) 뒤에 붙이시면 됩니다.!!
+var input = fs.readFileSync(filePath).toString().trim().split(' ').map(Number);
+
+//cording here
+let [hh,mm] = input;
+console.log((hh-9)*60+mm);
