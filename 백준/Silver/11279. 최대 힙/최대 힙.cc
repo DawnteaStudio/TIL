c0dc7779054804f4a88527c854438e3893c0a549
DawnteaStudio <90132183+DawnteaStudio@users.noreply.github.com>
2023-10-20 01:15:45 +0900

    [Silver II] Title: 최대 힙, Time: 16 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+#include <queue>
+using namespace std;
+
+int main()
+{
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+  int cases;
+  int command;
+  priority_queue<int> heap;
+
+  cin >> cases;
+  while(cases--)
+  {
+    cin >> command;
+    if (command == 0)
+    {
+      if (heap.empty())
+        cout << 0 << '\n';
+      else
+      {
+        cout << heap.top() << '\n';
+        heap.pop();
+      }
+    }
+    else
+      heap.push(command);
+  }
+}
