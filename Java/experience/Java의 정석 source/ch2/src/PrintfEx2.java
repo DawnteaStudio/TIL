4302d9ec4307e166f557e81eeeb3fedde61dad35
DawnteaStudio <dawnteabro@naver.com>
2024-01-11 20:52:11 +0900

    Create PrintfEx2.java

---
+++ Java/experience/Java의 정석 source/ch2/src/PrintfEx2.java
@@ -0,0 +1,10 @@
+public class PrintfEx2 {
+    public static void main(String[] args) {
+        String url = "www.codechobo.com";
+
+        float f1 = .10f; // .10f  vs .1f
+        System.out.println(f1);
+        float f2 = .1f;
+        System.out.println(f2);
+    }
+}
