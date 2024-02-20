ac0f25456374c99f1b6db831913402ff1d0d7da7
DawnteaStudio <90132183+DawnteaStudio@users.noreply.github.com>
2024-02-20 23:26:04 +0900

    [Bronze V] Title: Pyramids, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5341. Pyramids/Pyramids.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num;
+
+    while (1) {
+        cin >> num;
+        if (num == 0)
+            break ;
+        cout << num * (1 + num) / 2 << '\n';
+    }
+    return 0;
+}
