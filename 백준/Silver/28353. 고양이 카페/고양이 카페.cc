e573cffa2cc415474ecec5d3f0fef9ddd6a58c2f
DawnteaStudio <90132183+DawnteaStudio@users.noreply.github.com>
2024-03-16 20:06:51 +0900

    [Silver III] Title: 고양이 카페, Time: 0 ms, Memory: 2160 KB -BaekjoonHub

---
+++ 백준/Silver/28353. 고양이 카페/고양이 카페.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+#define fast ios::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+using namespace std;
+
+int main()
+{
+	fast;
+	int n, k, cnt = 0;
+	cin >> n >> k;
+
+	vector<int> v(n);
+	for (int i = 0; i < n; i++)
+		cin >> v[i];
+	sort(v.begin(), v.end());
+	int start = 0, end = n - 1;
+	
+	while (start < end)
+	{
+		if (v[start] + v[end] <= k)
+		{
+			cnt++;
+			start++;
+			end--;
+		}
+		else
+			end--;
+	}
+	cout << cnt;
+}
