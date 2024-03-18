e66a3344e3e3c83c38b608aedb24a0ae8f08b0d6
DawnteaStudio <90132183+DawnteaStudio@users.noreply.github.com>
2024-03-18 16:47:12 +0900

    [Bronze III] Title: 네 번째 점, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/3009. 네 번째 점/네 번째 점.cc
+++ 백준/Bronze/3009. 네 번째 점/네 번째 점.cc
@@ -1,75 +1,21 @@
 #include <iostream>
-#include <utility>
-
 #define fast ios::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
 using namespace std;
 
 int main()
 {
 	fast;
-	pair<int, int> x[2] = {{0, 0}}, y[2] = {{0, 0}};
-	int tmpX, tmpY, resX, resY;
+	int a[2], b[2], c[2], x, y;
 
-	for (int i = 0; i < 3; i++)
-	{
-		cin >> tmpX >> tmpY;
-		if (tmpX != x[0].first && tmpX != x[1].first)
-		{
-			if (x[0].first == 0)
-			{
-				x[0].first = tmpX;
-				x[0].second++;
-			}
-			else
-			{
-				x[1].first = tmpX;
-				x[1].second++;
-			}
-		}
-		else
-		{
-			if (tmpX == x[0].first)
-				x[0].second++;
-			else if (tmpX == x[1].first)
-				x[1].second++;
-			else
-			{
-				x[1].first = tmpX;
-				x[1].second++;
-			}
-		}
-		if (tmpY != y[0].first && tmpY != y[1].first)
-		{
-			if (y[0].first == 0)
-			{
-				y[0].first = tmpY;
-				y[0].second++;
-			}
-			else
-			{
-				y[1].first = tmpY;
-				y[1].second++;
-			}
-		}
-		else
-		{
-			if (tmpY == y[0].first)
-				y[0].second++;
-			else if (tmpY == y[1].first)
-				y[1].second++;
-			else
-			{
-				y[1].first = tmpY;
-				y[1].second++;
-			}
-		}	
-	}
-	for (int i = 0; i < 2; i++)
-	{
-		if (x[i].second == 1)
-			resX = x[i].first;
-		if (y[i].second == 1)
-			resY = y[i].first;
-	}
-	cout << resX << ' ' << resY;
+	cin >> a[0] >> a[1];
+	cin >> b[0] >> b[1];
+	cin >> c[0] >> c[1];
+	if (a[0] == b[0]) y = c[0];
+	else if (a[0] == c[0]) y = b[0];
+	else y = a[0];
+	if (a[1] == b[1]) x = c[1];
+	else if (a[1] == c[1]) x = b[1];
+	else x = a[1];
+	cout << y << ' ' << x;
+	return (0);
 }
