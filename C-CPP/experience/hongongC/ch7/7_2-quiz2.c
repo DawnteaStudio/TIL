7ecf76455c9a22d253a6b89b21bac0d86b501ec3
DawnteaStudio <dawnteabro@naver.com>
2024-03-22 16:13:45 +0900

    create 7_2-quiz2

---
+++ C-CPP/experience/hongongC/ch7/7_2-quiz2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void	sum(int num);
+
+int main(void)
+{
+	sum(10);
+	sum(100);
+	return (0);
+}
+
+void	sum(int num)
+{
+	int res = 0;
+
+	for (int i = 1; i <= num; i++)
+		res += i;
+	printf("1부터 %d까지의 합은 %d입니다.\n", num, res);
+}
