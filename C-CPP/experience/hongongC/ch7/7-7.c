051096f476e2df2f49cad63652b71f81f12858f1
DawnteaStudio <dawnteabro@naver.com>
2024-03-22 16:08:29 +0900

    create 7-7

---
+++ C-CPP/experience/hongongC/ch7/7-7.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void	fruit(int cnt);
+
+int main(void)
+{
+	fruit(0);
+	return (0);
+}
+
+void	fruit(int cnt)
+{
+	printf("apple\n");
+	if (cnt == 2)
+		return ;
+	fruit(cnt + 1);
+	printf("jam\n");
+}
