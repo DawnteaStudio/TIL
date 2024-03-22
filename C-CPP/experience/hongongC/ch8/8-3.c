b2eae7f4d1f8e426f5b9b473b0aecc0a63b05d5b
DawnteaStudio <dawnteabro@naver.com>
2024-03-22 21:51:41 +0900

    create 8-3

---
+++ C-CPP/experience/hongongC/ch8/8-3.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int arr[5];
+	int total = 0;
+	int count = sizeof(arr)/sizeof(arr[0]);
+	double avg;
+
+	for (int i = 0; i < count; i++)
+		scanf("%d", &arr[i]);
+	for (int i = 0; i < count; i++)
+	{
+		total += arr[i];
+		printf("%5d", arr[i]);
+	}
+	printf("\n");
+	avg = total / (double)count;
+
+	printf("평균 : %.1lf\n", avg);
+	return (0);
+}
