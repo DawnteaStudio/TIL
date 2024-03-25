3233ea591e3a9773c25ba14aca1cfc0d4344ce4b
Tea Green <90132183+DawnteaStudio@users.noreply.github.com>
2024-03-26 01:11:15 +0900

    [Bronze IV] Title: Sticky Keys, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/31656. Sticky Keys/Sticky Keys.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+#define fast ios::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+using namespace std;
+
+int main()
+{
+	string str;
+    char tmp = 0;
+    getline(cin, str);
+
+    for (int i = 0; i < (int)str.length(); i++)
+    {
+        if (str[i] != tmp)
+            cout << str[i];
+        tmp = str[i];
+    }
+}
