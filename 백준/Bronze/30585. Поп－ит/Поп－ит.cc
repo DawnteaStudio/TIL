15ce00596f1fc1560e6daa0c09d19c9aa4f3afcb
Tea Green <90132183+DawnteaStudio@users.noreply.github.com>
2024-04-07 17:45:17 +0900

    [Bronze IV] Title: Поп-ит, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/30585. Поп－ит/Поп－ит.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#define fast ios::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+using namespace std;
+
+int main()
+{
+	fast;
+	int n, m;
+	string tmp;
+	cin >> n >> m;
+	int red = 0, blue = 0;
+
+	for (int i = 0; i < n; i++)
+	{
+		cin >> tmp;
+		for (int j = 0; j < m; j++)
+		{
+			if (tmp[j] == '0')
+				red++;
+			else
+				blue++;
+		}
+	}
+	cout << (red < blue ? red  : blue);
+}
