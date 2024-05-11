10e2d4a2e0b9f9545f54aad433c00d29b1eddac0
Tea Green <90132183+DawnteaStudio@users.noreply.github.com>
2024-05-11 14:55:30 +0900

    [Bronze IV] Title: Случай с игрой, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/29267. Случай с игрой/Случай с игрой.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#define fast ios::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+using namespace std;
+
+int main()
+{
+	fast;
+	int n, k, save = 0, now = 0;
+	string cmd;
+	cin >> n >> k;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> cmd;
+		if (cmd == "ammo")
+			now += k;
+		else if (cmd == "save")
+			save = now;
+		else if (cmd == "shoot")
+			now -= 1;
+		else if (cmd == "load")
+			now = save;
+		cout << now << '\n';
+	}
+}
