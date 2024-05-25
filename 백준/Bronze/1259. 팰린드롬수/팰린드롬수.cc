c1809d3177270b869b524439155b37b11dcdd717
Tea Green <90132183+DawnteaStudio@users.noreply.github.com>
2024-05-26 00:08:05 +0900

    [Bronze I] Title: 팰린드롬수, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/1259. 팰린드롬수/팰린드롬수.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#define fast ios::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+using namespace std;
+
+int main()
+{
+	fast;
+	string str;
+	string tmp;
+	while(true)
+	{
+		cin >> str;
+		tmp = str;
+		if (str == "0")
+			break;
+		reverse(str.begin(), str.end());
+		if (str == tmp)
+			cout << "yes\n";
+		else
+			cout << "no\n";
+	}
+}
