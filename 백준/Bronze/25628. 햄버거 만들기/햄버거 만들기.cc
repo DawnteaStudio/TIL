c9c275bac77e2ef4fe4fc640d6ea9ad4aba0b841
Tea Green <90132183+DawnteaStudio@users.noreply.github.com>
2024-05-26 00:07:55 +0900

    [Bronze IV] Title: 햄버거 만들기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25628. 햄버거 만들기/햄버거 만들기.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#define fast ios::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+using namespace std;
+
+int main()
+{
+	fast;
+	int n, m, res = 0;
+	cin >> n >> m;
+	if (n >= 2 * m)
+		cout << m;
+	else
+		cout << (n / 2);
+}
