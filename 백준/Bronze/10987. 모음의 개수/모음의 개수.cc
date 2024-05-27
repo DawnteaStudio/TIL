c100d1fcc80f4608c808df1b15e15f9e6176349a
Tea Green <90132183+DawnteaStudio@users.noreply.github.com>
2024-05-27 23:02:06 +0900

    [Bronze IV] Title: 모음의 개수, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10987. 모음의 개수/모음의 개수.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#define fast ios::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+using namespace std;
+
+int main()
+{
+	fast;
+	string str;
+	cin >> str;
+	int res = 0;
+	for (int i = 0; i < str.length(); i++)
+	{
+		if (str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u')
+			res++;
+	}
+	cout << res;
+}
