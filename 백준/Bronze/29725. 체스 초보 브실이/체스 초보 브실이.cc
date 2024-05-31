dcf18f9be2bac9944902d1071490cae58dc56c7a
Tea Green <90132183+DawnteaStudio@users.noreply.github.com>
2024-05-31 23:49:49 +0900

    [Bronze IV] Title: 체스 초보 브실이, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/29725. 체스 초보 브실이/체스 초보 브실이.cc
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+#define fast ios::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+using namespace std;
+
+int main()
+{
+	fast;
+	int black = 0, white = 0;
+    string input;
+    for (int i = 0; i < 8; i++)
+    {
+        cin >> input;
+        for (int j = 0; j < 8; j++)
+        {
+            if (input[j] == 'P')
+                white += 1;
+            else if (input[j] == 'N')
+                white += 3;
+            else if (input[j] == 'B')
+                white += 3;
+            else if (input[j] == 'R')
+                white += 5;
+            else if (input[j] == 'Q')
+                white += 9;
+            else if (input[j] == 'p')
+                black += 1;
+            else if (input[j] == 'n')
+                black += 3;
+            else if (input[j] == 'b')
+                black += 3;
+            else if (input[j] == 'r')
+                black += 5;
+            else if (input[j] == 'q')
+                black += 9;
+        }
+    }
+    cout << white - black;
+}
