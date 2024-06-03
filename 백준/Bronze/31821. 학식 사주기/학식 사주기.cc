aee2be47fa98c6f79f258f46231ce0eae426658b
Tea Green <90132183+DawnteaStudio@users.noreply.github.com>
2024-06-03 22:30:34 +0900

    [Bronze IV] Title: 학식 사주기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/31821. 학식 사주기/학식 사주기.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#define fast ios::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+using namespace std;
+
+int main()
+{
+	fast;
+	int n, res = 0;
+	cin >> n;
+	int arr[n];
+	for (int i = 0; i < n; i++)
+		cin >> arr[i];
+
+	cin >> n;
+	int index;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> index;
+		res += arr[index - 1];
+	}
+	cout << res;
+}
