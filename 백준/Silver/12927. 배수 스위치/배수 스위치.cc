75fb4400dc8130ad6abc65eb9ad329d57e0567de
Tea Green <90132183+DawnteaStudio@users.noreply.github.com>
2024-06-17 22:49:08 +0900

    [Silver IV] Title: 배수 스위치, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/12927. 배수 스위치/배수 스위치.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+#define fast ios::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+using namespace std;
+
+void	click(int index, string &str)
+{
+	int add = index + 1;
+	for (int i = index; i < str.length(); i += add)
+	{
+		if (str[i] == 'Y')
+			str[i] = 'N';
+		else
+			str[i] = 'Y';
+	}
+}
+
+int main()
+{
+	fast;
+	string light;
+	int res = 0;
+	cin >> light;
+
+	for (int i = 0; i < light.length(); i++)
+	{
+		if (light[i] == 'Y')
+		{
+			click(i, light);
+			res++;
+		}
+	}
+	cout << res;
+}
