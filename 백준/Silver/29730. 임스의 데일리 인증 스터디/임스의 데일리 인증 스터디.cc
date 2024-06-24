b5136a2f21406837422085f5e3c9557de19625b2
Tea Green <90132183+DawnteaStudio@users.noreply.github.com>
2024-06-24 22:32:46 +0900

    [Silver III] Title: 임스의 데일리 인증 스터디, Time: 0 ms, Memory: 2164 KB -BaekjoonHub

---
+++ 백준/Silver/29730. 임스의 데일리 인증 스터디/임스의 데일리 인증 스터디.cc
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#define fast ios::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+using namespace std;
+
+bool	cmp(string a, string b)
+{
+	if (a.length() < b.length())
+		return true;
+	else if (a.length() == b.length())
+		return a < b;
+	return false;
+}
+
+bool	cmp2(string a, string b)
+{
+	int num1 = stoi(a.substr(7));
+	int num2 = stoi(b.substr(7));
+	return num1 < num2;
+}
+
+int main()
+{
+	fast;
+	int n;
+	cin >> n;
+	cin.ignore();
+	vector<string> v;
+	vector<string> b;
+	string tmp;
+	for (int i = 0; i < n; i++)
+	{
+		getline(cin, tmp);
+		if (tmp.length() > 4 && tmp[3] == '.')
+			b.push_back(tmp);
+		else
+			v.push_back(tmp);
+	}
+	sort(v.begin(), v.end(), cmp);
+	sort(b.begin(), b.end(), cmp2);
+	for (int i = 0; i < v.size(); i++)
+		cout << v[i] << '\n';
+	for (int i = 0; i < b.size(); i++)
+		cout << b[i] << '\n';
+}
