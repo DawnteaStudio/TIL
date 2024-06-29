a6d25930a3b410af0b4329ed1f3f51b2f76d0935
Tea Green <90132183+DawnteaStudio@users.noreply.github.com>
2024-06-29 20:42:03 +0900

    [Silver V] Title: 젓가락, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/24228. 젓가락/젓가락.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+#define fast ios::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+using namespace std;
+
+int main()
+{
+	fast;
+	long long n, r;
+	cin >> n >> r;
+	cout << n + (2 * r) - 1;
+}
