db885260456c025b6708fe65edeb680f820d3c4e
Tea Green <90132183+DawnteaStudio@users.noreply.github.com>
2024-07-05 23:11:00 +0900

    [Bronze IV] Title: 녹색거탑, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24723. 녹색거탑/녹색거탑.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#define fast ios::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+using namespace std;
+
+int main()
+{
+	fast;
+	int	n, res = 1;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+		res *= 2;
+	cout << res;
+}
