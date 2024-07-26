c5370c0132a713ccd2d3f2531ea5491add96df38
Tea Green <90132183+DawnteaStudio@users.noreply.github.com>
2024-07-27 04:08:38 +0900

    [Bronze IV] Title: 주사위, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/9295. 주사위/주사위.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#define fast ios::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+using namespace std;
+
+int main()
+{
+	fast;
+	int n, a, b;
+	cin >> n;
+	
+	for (int i = 1; i <= n; i++)
+	{
+		cin >> a >> b;
+		cout << "Case " << i << ": " << a + b << '\n';
+	}
+}
