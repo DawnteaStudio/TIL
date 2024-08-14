9fa621d266203356447b2d91010375212ac982ca
Tea Green <90132183+DawnteaStudio@users.noreply.github.com>
2024-08-14 17:59:26 +0900

    [Bronze II] Title: XORChic, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/17285. XORChic/XORChic.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#define fast ios::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+using namespace std;
+
+int main()
+{
+	fast;
+	string hello;
+	cin >> hello;
+
+	int key;
+	key = hello[0] ^ 'C';
+	for (int i = 0; i < hello.length(); i++)
+		cout << (char)(hello[i] ^ key);
+}
