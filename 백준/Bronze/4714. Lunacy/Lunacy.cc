723efbe95a6daf2d7aec428186578c7c92b91a4f
Tea Green <90132183+DawnteaStudio@users.noreply.github.com>
2024-09-18 21:11:45 +0900

    [Bronze IV] Title: Lunacy, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/4714. Lunacy/Lunacy.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#define fast ios::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+using namespace std;
+
+int main()
+{
+	fast;
+	double input;
+	cout.precision(2);
+	cout << fixed;
+	while (true)
+	{
+		cin >> input;
+		if (input < 0)
+			break;
+		cout << "Objects weighing " << input <<" on Earth will weigh " << input * 0.167 << " on the moon.\n";
+	}
+}
