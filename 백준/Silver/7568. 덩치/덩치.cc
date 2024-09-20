5b35dc1297174d7c3d81f058e1cfa6a6003df32c
Tea Green <90132183+DawnteaStudio@users.noreply.github.com>
2024-09-21 00:42:06 +0900

    [Silver V] Title: 덩치, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/7568. 덩치/덩치.cc
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#define fast ios::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+using namespace std;
+
+struct human
+{
+	int cm;
+	int kg;
+	int rank;
+};
+
+int main() {
+	fast;
+	int n, cm, kg, rank;
+	cin >> n;
+
+	vector<human> vec(n);
+	for (int i = 0; i < n; i++)
+	{
+		cin >> cm >> kg;
+		vec[i].cm = cm;
+		vec[i].kg = kg;
+	}
+	for (int i = 0; i < n; i++)
+	{
+		rank = 1;
+		for (int j = 0; j < n; j++)
+		{
+			if (i == j)
+				continue;
+			if (vec[i].cm < vec[j].cm && vec[i].kg < vec[j].kg)
+				rank++;
+		}
+		vec[i].rank = rank;
+	}
+	for (int i = 0; i < n; i++)
+		cout << vec[i].rank << " ";
+}
