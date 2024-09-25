b913ca2d03459249be44eb1056ae51fd752bd4e2
Tea Green <90132183+DawnteaStudio@users.noreply.github.com>
2024-09-26 02:54:38 +0900

    [Bronze III] Title: 직사각형에서 탈출, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1085. 직사각형에서 탈출/직사각형에서 탈출.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#define fast ios::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+using namespace std;
+
+int main() {
+	fast;
+	int x, y, w, h, res;
+	cin >> x >> y >> w >> h;
+
+	res = x > y ? y : x;
+	res = res > w - x ? w - x : res;
+	res = res > h - y ? h - y : res;
+	cout << res;
+}
