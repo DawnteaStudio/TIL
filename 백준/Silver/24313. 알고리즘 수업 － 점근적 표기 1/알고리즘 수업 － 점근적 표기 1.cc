527d1a42936afdc46e437775ef5c6eb9fb12c54c
Tea Green <90132183+DawnteaStudio@users.noreply.github.com>
2024-09-26 02:54:51 +0900

    [Silver V] Title: 알고리즘 수업 - 점근적 표기 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/24313. 알고리즘 수업 － 점근적 표기 1/알고리즘 수업 － 점근적 표기 1.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#define fast ios::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+using namespace std;
+
+int main() {
+	fast;
+	int a1, a0, c, n0;
+	cin >> a1 >> a0 >> c >> n0;
+
+	if ((c - a1) * n0 - a0 >= 0)
+	{
+		if (c < a1)
+			cout << 0;
+		else
+			cout << 1;
+	}
+	else
+		cout << 0;
+}
