c1610b6bc58bdfca73e4877ac3a3d6f97ab47650
Tea Green <90132183+DawnteaStudio@users.noreply.github.com>
2024-10-05 18:26:16 +0900

    [Bronze I] Title: Last Word, Time: 72 ms, Memory: 3680 KB -BaekjoonHub

--- 백준/Bronze/17576. Last Word/Last Word.cc
+++ 백준/Bronze/17576. Last Word/Last Word.cc
@@ -11,16 +11,13 @@ int main()
 	int n;
 	cin >> n;
 	int start = 0;
-	int len = str.length();
+
 	int a, b;
 	for (int i = 0; i < n; i++)
 	{
 		cin >> a >> b;
 		start += a;
 	}
-	if (start + b < len)
-		str = str.substr(start, b);
-	else
-		str = str.substr(start);
+	str = str.substr(start, b);
 	cout << str;
 }
