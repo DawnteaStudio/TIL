71743c56d37c5d0d4f2cd40ee00afa86d439d8a0
Tea Green <90132183+DawnteaStudio@users.noreply.github.com>
2024-11-05 01:33:11 +0900

    [Silver V] Title: 팩토리얼 0의 개수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+#define fast ios::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+using namespace std;
+
+int main()
+{
+	fast;
+	int n, res = 1, cnt = 0;
+	cin >> n;
+
+	for (int i = 1; i <= n; i++)
+	{
+		res *= i;
+		while (res % 10 == 0)
+		{
+			cnt++;
+			res /= 10;
+		}
+		if (i < 10)
+			res %= 100;
+		else if (i < 100)
+			res %= 10000;
+		else
+			res %= 1000000;
+	}
+	cout << cnt;
+}
