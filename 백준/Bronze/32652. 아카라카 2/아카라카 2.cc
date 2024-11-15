443880cb6495dcb0cd71bfab854d6c5150bb496b
Tea Green <90132183+DawnteaStudio@users.noreply.github.com>
2024-11-15 22:37:18 +0900

    [Bronze III] Title: 아카라카 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/32652. 아카라카 2/아카라카 2.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#define fast ios::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+using namespace std;
+
+int main() {
+	fast;
+	int n;
+	cin >> n;
+	
+	string res = "AKARAKA";
+	for (int i = 1; i < n; i++)
+		res += "RAKA";
+	cout << res;
+}
