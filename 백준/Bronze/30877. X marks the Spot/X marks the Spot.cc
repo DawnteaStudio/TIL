18f5bb8779c6f8e4b0187ab4ff35a022e7be9d10
Tea Green <90132183+DawnteaStudio@users.noreply.github.com>
2024-11-15 22:37:32 +0900

    [Bronze II] Title: X marks the Spot, Time: 56 ms, Memory: 3592 KB -BaekjoonHub

---
+++ 백준/Bronze/30877. X marks the Spot/X marks the Spot.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#define fast ios::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+using namespace std;
+
+int main() {
+	fast;
+	int n;
+	cin >> n;
+
+	string input, res;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> input;
+		int pos = input.find('x');
+		if (pos == string::npos)
+			pos = input.find('X');
+		cin >> input;
+		if (input[pos] >= 'a' & input[pos] <= 'z')
+			res += input[pos] + ('A' - 'a');
+		else
+			res += input[pos];
+	}
+	cout << res;
+}
