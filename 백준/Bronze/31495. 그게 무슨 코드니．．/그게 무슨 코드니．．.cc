24c769193f5f5838a1c4ae00ebc77caa8af62d98
Tea Green <90132183+DawnteaStudio@users.noreply.github.com>
2024-11-30 23:43:44 +0900

    [Bronze IV] Title: 그게 무슨 코드니.., Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/31495. 그게 무슨 코드니．．/그게 무슨 코드니．．.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#define fast ios::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+using namespace std;
+
+int main()
+{
+	fast;
+	string str;
+	getline(cin, str);
+
+	if (str.length() > 2 && str[0] == '\"' && str[str.length() - 1] == '\"')
+		cout << str.substr(1, str.length() - 2);
+	else
+		cout << "CE";
+}
