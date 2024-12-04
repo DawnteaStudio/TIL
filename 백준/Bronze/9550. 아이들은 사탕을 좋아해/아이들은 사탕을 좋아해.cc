50ef350abee2bac5522bd235df31b52b852d7564
Tea Green <90132183+DawnteaStudio@users.noreply.github.com>
2024-12-04 14:54:06 +0900

    [Bronze III] Title: 아이들은 사탕을 좋아해, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/9550. 아이들은 사탕을 좋아해/아이들은 사탕을 좋아해.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#define fast ios::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+using namespace std;
+
+int main()
+{
+	fast;
+	int n, res, input;
+	cin >> n;
+
+	int candy, k;
+	for (int i = 0; i < n; i++)
+	{
+		res = 0;
+		cin >> candy >> k;
+		for (int j = 0; j < candy; j++)
+		{
+			cin >> input;
+			res += input / k;
+		}
+		cout << res << '\n';
+	}
+}
