bf660e043bcdb77603206af49fb2f3c297284011
Tea Green <90132183+DawnteaStudio@users.noreply.github.com>
2024-12-22 22:18:39 +0900

    [Bronze IV] Title: Contest Timing, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5928. Contest Timing/Contest Timing.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#define fast ios::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+using namespace std;
+
+int main()
+{
+	fast;
+	int d, h, m;
+	cin >> d >> h >> m;
+
+	int t = 11 * 24 * 60 + 11 * 60 + 11;
+	int my_t = d * 24 * 60 + h * 60 + m;
+	if (t > my_t)
+		cout << -1;
+	else
+		cout << my_t - t;
+}
