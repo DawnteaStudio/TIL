a8d0f8a249d856aadf7773a0e0777a73deca34d4
Tea Green <90132183+DawnteaStudio@users.noreply.github.com>
2025-02-13 02:16:24 +0900

    [Bronze II] Title: Silent Auction, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/21613. Silent Auction/Silent Auction.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#define fast ios::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+using namespace std;
+
+int main()
+{
+	fast;
+	string res, tmp;
+	int input, cost;
+
+	cost = 0;
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> tmp >> input;
+		if (input > cost) {
+			res = tmp;
+			cost = input;
+		}
+	}
+	cout << res;
+}
