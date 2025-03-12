39dacca2eb725fd191d0565806bbf57274f8ab35
Tea Green <90132183+DawnteaStudio@users.noreply.github.com>
2025-03-12 18:04:08 +0900

    [Silver IV] Title: 가장 긴 단어, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/5637. 가장 긴 단어/가장 긴 단어.cc
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+#define fast ios::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+using namespace std;
+
+int main()
+{
+	fast;
+	string str, tmp_s, res;
+	while (true) {
+		cin >> str;
+		if (str == "E-N-D")
+			break;
+		int len = str.length();
+		int tmp = 0;
+		for (int i = 0; i < len; i++) {
+			if (str[i] == '-' || (str[i] >= 'a' && str[i] <= 'z') || (str[i] >= 'A' && str[i] <= 'Z')) {
+				tmp++;
+				tmp_s += str[i];
+			}
+			else {
+				if (res.length() < tmp_s.length())
+					res = tmp_s;
+				tmp_s = "";
+				tmp = 0;
+			}
+		}
+		if (res.length() < tmp_s.length())
+		res = tmp_s;
+		tmp_s = "";
+		tmp = 0;
+	}
+	for (int i = 0; i < (int)res.length(); i++) {
+		if (res[i] == '-' || (res[i] >= 'a' && res[i] <= 'z'))
+			cout << res[i];
+		else
+			cout << (char)(res[i] - 'A' + 'a');
+	}
+}
