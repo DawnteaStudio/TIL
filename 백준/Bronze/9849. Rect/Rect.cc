1c389eee68762a1394bfd0b415eb705109ca927e
Tea Green <90132183+DawnteaStudio@users.noreply.github.com>
2025-04-10 00:55:42 +0900

    [Bronze I] Title: Rect, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/9849. Rect/Rect.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+#define fast ios::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+using namespace std;
+
+int main()
+{
+	fast;
+	int n;
+	cin >> n;
+	int lx, rx, ly, ry;
+	int tmp1, tmp2, tmp3, tmp4;
+	for (int i = 0; i < n; i++) {
+		cin >> tmp1 >> tmp2 >> tmp3 >> tmp4;
+		if (i == 0) {
+			lx = tmp1;
+			rx = tmp2;
+			ly = tmp3;
+			ry = tmp4;
+		}
+		else {
+			lx = max(lx, tmp1);
+			rx = min(rx, tmp2);
+			ly = max(ly, tmp3);
+			ry = min(ry, tmp4);
+		}
+	}
+	if (lx > rx || ly > ry)
+		cout << "0\n";
+	else
+		cout << (rx - lx) * (ry - ly) << "\n";
+}
