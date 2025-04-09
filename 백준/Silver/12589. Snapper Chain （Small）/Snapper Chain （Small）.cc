7961203c6ed0c4a06adb5620730b85021fd4c610
Tea Green <90132183+DawnteaStudio@users.noreply.github.com>
2025-04-10 00:54:50 +0900

    [Silver V] Title: Snapper Chain (Small), Time: 0 ms, Memory: 2208 KB -BaekjoonHub

---
+++ 백준/Silver/12589. Snapper Chain （Small）/Snapper Chain （Small）.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cmath>
+#define fast ios::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+using namespace std;
+
+int main()
+{
+	fast;
+	int t;
+	cin >> t;
+	int n , k;
+	long long ans;
+	for (int i = 1; i <= t; i++) {
+		cin >> n >> k;
+		ans = pow(2, n);
+		if (k == 0)
+			cout << "Case #" << i << ": OFF\n";
+		else if (ans > k + 1)
+			cout << "Case #" << i << ": OFF\n";
+		else if ((k + 1) % ans == 0)
+			cout << "Case #" << i << ": ON\n";
+		else
+			cout << "Case #" << i << ": OFF\n";
+	}
+}
