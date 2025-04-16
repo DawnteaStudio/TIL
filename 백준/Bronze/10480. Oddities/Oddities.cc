00d9a6c28c1dbdabbed37c07070afecca9505d59
Tea Green <90132183+DawnteaStudio@users.noreply.github.com>
2025-04-16 23:30:50 +0900

    [Bronze IV] Title: Oddities, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10480. Oddities/Oddities.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#define fast ios::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL)
+
+using namespace std;
+
+int main() {
+	fast;
+	int n, num;
+	cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		cin >> num;
+		if (num == 0 || num % 2 == 0)
+			cout << num << " is even\n";
+		else
+			cout << num << " is odd\n";
+	}
+}
