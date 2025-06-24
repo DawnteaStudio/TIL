806423c6c851850e7ab135c8a2aacd41711cf58f
DawnteaStudio <dawnteabro@naver.com>
2025-06-24 17:15:30 +0900

    16171 solve

---
+++ coding-test/Baekjoon/16000/16171.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#define fast ios::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+using namespace std;
+
+int main()
+{
+	fast;
+	string s, k; cin >> s >> k;
+	string tmp;
+	for (int i=0; i<(int)s.size(); i++) {
+		if (s[i] >= '0' && s[i] <= '9') continue ;
+		tmp += s[i];
+	}
+	cout << (tmp.find(k) != string::npos);
+}
