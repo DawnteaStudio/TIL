ce3bac43287601f2f9981abf115fb4b499bc4b01
Tea Green <90132183+DawnteaStudio@users.noreply.github.com>
2025-08-27 00:23:52 +0900

    Title: 연속되는 수 2, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250827/연속되는 수 2/continuous-number2.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int N;
+int arr[1000];
+
+int main() {
+    cin >> N;
+    for (int i = 0; i < N; i++) {
+        cin >> arr[i];
+    }
+
+    // Please write your code here.
+
+    int tmp = 0, cnt = 0;
+
+    for (int i = 0; i < N; i++) {
+        if (i == 0 || arr[i] == arr[i - 1])
+            tmp++;
+        else
+            tmp = 1;
+        if (tmp > cnt)
+            cnt = tmp;
+    }
+    cout << cnt;
+    return 0;
+}
