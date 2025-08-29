e541f6b6a1770b3c5b9c92beedbe876144017eac
Tea Green <90132183+DawnteaStudio@users.noreply.github.com>
2025-08-29 10:07:02 +0900

    Title: 문자에 따른 명령 2, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250829/문자에 따른 명령 2/text-based-commands2.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+int dy[4] = {1, 0, -1, 0};
+int dx[4] = {0, 1, 0, -1};
+int dir, y, x;
+
+string dirs;
+
+int main() {
+    cin >> dirs;
+
+    // Please write your code here.
+    int len = dirs.length();
+
+    for (int i = 0; i < len; i++) {
+        if (dirs[i] == 'F') {
+            y += dy[dir];
+            x += dx[dir];
+        }
+        else if (dirs[i] == 'R')
+            dir = (dir + 1) % 4;
+        else {
+            dir -= 1;
+            if (dir == 0)
+                dir = 3;
+        }
+    }
+    cout << y << " " << x;
+    return 0;
+}
