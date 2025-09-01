3ab4fe66b481bccb950e35cfe9705d84b45af234
Tea Green <90132183+DawnteaStudio@users.noreply.github.com>
2025-09-01 17:28:06 +0900

    15652 solve

---
+++ coding-test/Baekjoon/15000/15652.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#define fast ios::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+using namespace std;
+
+int n, m;
+
+void solve(int depth, vector<int> &arr)
+{
+	if (depth == m) {
+		for (int i = 0; i < m; i++)
+			cout << arr[i] << " ";
+		cout << '\n';
+		return;
+	}
+
+	int start = 1;
+	if (depth)
+		start = arr[depth - 1];
+	for (int i = start; i <= n; i++) {
+		arr[depth] = i;
+		solve(depth + 1, arr);
+	}
+}
+
+int main()
+{
+	fast;
+	cin >> n >> m;
+	vector<int> arr(n);
+	solve(0, arr);
+}
