ca21ca6f6f6961ec600cf2c08756292c8e8635f8
Tea Green <90132183+DawnteaStudio@users.noreply.github.com>
2025-09-01 17:29:06 +0900

    15655 solve
    Implement backtracking solution for combination generation.

---
+++ coding-test/Baekjoon/15000/15655.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <set>
+#define fast ios::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+using namespace std;
+
+int n, m;
+
+void solve(int depth, vector<int> &arr, set<int> &visited, vector<int> &nums, int index)
+{
+	if (depth == m) {
+		for (int i = 0; i < m; i++)
+			cout << arr[i] << " ";
+		cout << '\n';
+		return;
+	}
+
+	for (int i = index + 1; i < n; i++) {
+		if (visited.find(nums[i]) != visited.end())
+			continue;
+		visited.insert(nums[i]);
+		arr[depth] = nums[i];
+		solve(depth + 1, arr, visited, nums, i);
+		visited.erase(nums[i]);
+	}
+}
+
+int main()
+{
+	fast;
+	cin >> n >> m;
+	vector<int> nums(n);
+	for (int i = 0; i < n; i++)
+		cin >> nums[i];
+	sort(nums.begin(), nums.end());
+	vector<int> arr(n);
+	set<int> visited;
+	solve(0, arr, visited, nums, -1);
+}
