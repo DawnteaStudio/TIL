6bce63e3d2294189893216eb027cd0131c700c70
DawnteaStudio <dawnteabro@naver.com>
2025-10-09 02:07:45 +0900

    6376 solve

---
+++ coding-test/Baekjoon/6000/6376.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#define fast ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr)
+using namespace std;
+
+int main() {
+    fast;
+    cout << R"(n e
+- -----------
+0 1
+1 2
+2 2.5
+3 2.666666667
+4 2.708333333
+5 2.716666667
+6 2.718055556
+7 2.718253968
+8 2.718278770
+9 2.718281526)";
+    return 0;
+}
