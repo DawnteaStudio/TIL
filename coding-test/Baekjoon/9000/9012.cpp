3190c6cee6d8c1e176b08e5fb5b6841ec882f239
GitHub Action <action@github.com>
2025-10-29 15:05:54 +0900

    9012 solve

--- coding-test/Baekjoon/9000/9012.cpp
+++ coding-test/Baekjoon/9000/9012.cpp
@@ -7,34 +7,34 @@ using namespace std;
 
 int main()
 {
-    fast;
-    int n, flag;
-    string str;
-    stack<char> s;
+	fast;
+	int	n, flag;
+	string str;
+	stack<char> s;
 
-    cin >> n;
-    flag = 0;
-    for (int i = 0; i < n; i++)
-    {
-        cin >> str;
-        flag = 0;
-        for (int j = 0; j < str.length(); j++)
-        {
-            if (str[j] == '(')
-                s.push('(');
-            else if (s.empty())
-            {
-                flag = 1;
-                break;
-            }
-            else
-                s.pop();
-        }
-        if (s.empty() && flag == 0)
-            cout << "YES\n";
-        else
-            cout << "NO\n";
-        while (!s.empty())
-            s.pop();
-    }
+	cin >> n;
+	flag = 0;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> str;
+		flag = 0;
+		for (int j = 0; j < str.length(); j++)
+		{
+			if (str[j] == '(')
+				s.push('(');
+			else if (s.empty())
+			{
+				flag = 1;
+				break;
+			}
+			else
+				s.pop();
+		}
+		if (s.empty() && flag == 0)
+			cout << "YES\n";
+		else
+			cout << "NO\n";
+		while (!s.empty())
+			s.pop();
+	}
 }
