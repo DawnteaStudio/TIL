c622b0650ea3ec514d2c26c40224b41ce332a1f6
Tea Green <90132183+DawnteaStudio@users.noreply.github.com>
2025-12-09 23:38:20 +0900

    [Bronze IV] Title: 三連続 (Three Consecutive), Time: 0 ms, Memory: 2024 KB, Score: 40 point -BaekjoonHub

---
+++ 백준/Bronze/31636. 三連続 （Three Consecutive）/三連続 （Three Consecutive）.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+#define fast ios::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+using namespace std;
+
+int main()
+{
+	fast;
+	int n;
+	cin >> n;
+	string str;
+	cin >> str;
+
+	if (n < 3)
+		cout << "No";
+	else {
+		int tmp = 0;
+		for (int i = 0; i < n; i++) {
+			if (str[i] == 'o')
+				tmp++;
+			else
+				tmp = 0;
+			if (tmp == 3) {
+				cout << "Yes";
+				return 0;
+			}
+		}
+	}
+	cout << "No";
+}
