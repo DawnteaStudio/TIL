468a3d02dd1253e07cd7b270a676ad7c6d1ad412
Tea Green <90132183+DawnteaStudio@users.noreply.github.com>
2025-12-24 18:27:14 +0900

    [Bronze IV] Title: 민규의 서카디안 리듬, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/34400. 민규의 서카디안 리듬/민규의 서카디안 리듬.cc
@@ -0,0 +1,24 @@
+// 복붙용
+#include <iostream>
+#define fast ios::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+using namespace std;
+
+int main()
+{
+    fast;
+    int t;
+    cin >> t;
+
+    for (int i = 0; i < t; i++) {
+        int tmp;
+        cin >> tmp;
+
+        tmp %= 25;
+        if (tmp < 17)
+            cout << "ONLINE\n";
+        else
+            cout << "OFFLINE\n";
+    }
+
+    return (0);
+}
