d9b2350a3fc488f744d43b23144cc842accee941
Tea Green <90132183+DawnteaStudio@users.noreply.github.com>
2026-01-04 23:28:54 +0900

    [Bronze IV] Title: Acquiring SW-IT Corn, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/34529. Acquiring SW－IT Corn/Acquiring SW－IT Corn.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+#define fast ios::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+using namespace std;
+
+int main()
+{
+    fast;
+	int x, y, z, u, v, w;
+	cin >> x >> y >> z >> u >> v >> w;
+	cout << x * (u / 100) + y * (v / 50) + z * (w / 20);
+}
