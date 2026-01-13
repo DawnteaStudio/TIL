00c5df3033a2ac27a1b2d146e904b33e2c829818
Tea Green <90132183+DawnteaStudio@users.noreply.github.com>
2026-01-13 23:19:38 +0900

    [Bronze IV] Title: 散歩 (Walking), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/33162. 散歩 （Walking）/散歩 （Walking）.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#define fast ios::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+using namespace std;
+
+int main()
+{
+    fast;
+	int n;
+	cin >> n;
+	if (n % 2 == 0)
+		cout << n / 2;
+	else
+		cout << n / 2 + 3;
+}
