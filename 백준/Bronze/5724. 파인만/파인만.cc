c323192e9f2d632087562ea26283571a9d477e47
Tea Green <90132183+DawnteaStudio@users.noreply.github.com>
2026-01-29 21:55:05 +0900

    [Bronze III] Title: 파인만, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5724. 파인만/파인만.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cmath>
+#define fast ios::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+using namespace std;
+
+int main()
+{
+	fast;
+	int n;
+	while (true) {
+		cin >> n;
+		if (n == 0)
+            break ;
+		int cnt = 0;
+		for (int j=n; j>=1; j--)
+			cnt += (int)pow(j, 2);
+		cout << cnt << '\n';
+	}
+}
