8072344e124d20a2c2c039c6866a2a5ed94c00c6
Tea Green <90132183+DawnteaStudio@users.noreply.github.com>
2026-02-09 18:16:04 +0900

    [level 0] Title: 대소문자 바꿔서 출력하기, Time: 186.27 ms, Memory: 72.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181949. 대소문자 바꿔서 출력하기/대소문자 바꿔서 출력하기.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        
+        int len = a.length();
+        for (int i = 0; i < len; i++) {
+            if (a.charAt(i) >= 'a' && a.charAt(i) <= 'z')
+                System.out.print((char)(a.charAt(i) - 'a' + 'A'));
+            else if (a.charAt(i) >= 'A' && a.charAt(i) <= 'Z')
+                System.out.print((char)(a.charAt(i) - 'A' + 'a'));
+        }
+    }
+}
