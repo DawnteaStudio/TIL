ed9d7e1df649eeef983e39fc9cdd8a5425a7a734
Tea Green <90132183+DawnteaStudio@users.noreply.github.com>
2026-03-07 16:58:51 +0900

    [Bronze III] Title: 도미노, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2921. 도미노/도미노.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#define fast ios::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+using namespace std;
+
+int main()
+{
+	fast;
+	int n;
+    cin >> n;
+	int ans = 0;
+	for (int i=0; i<=n; i++) {
+		for (int j=i; j<=n; j++)
+            ans += i + j;
+	}
+	cout << ans;
+}
