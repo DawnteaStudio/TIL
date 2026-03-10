fd4ab0013b82210b645ce51807df31b512e10446
GitHub Action <action@github.com>
2026-03-11 02:34:33 +0900

    6198 solve

--- coding-test/Baekjoon/6000/6198.cpp
+++ coding-test/Baekjoon/6000/6198.cpp
@@ -1,41 +1,26 @@
 #include <iostream>
 #include <stack>
-#include <utility>
 #define fast ios::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
 using namespace std;
 
 int main()
 {
-    fast;
-    int n;
-    cin >> n;
+	fast;
+	int n, tmp;
+	long long res = 0;
+	cin >> n;
 
-    stack<int> s;
-    stack<pair<int, int> > dp;
-    for (int i = 0; i < n; i++) {
-        int tmp;
-        cin >> tmp;
-        s.push(tmp);
-    }
-
-    long long res = 0;
-    
-    for (int i = 0; i < n; i++) {
-        int cnt = 0;
-        int top = s.top();
-        s.pop();
-
-        while (!dp.empty() && dp.top().first < top) {
-            res += dp.top().second;
-            cnt += dp.top().second + 1;
-            dp.pop();
-        }
-        dp.push({top, cnt});
-    }
-
-    while (!dp.empty()) {
-        res += dp.top().second;
-        dp.pop();
-    }
-    cout << res;
+	stack<int> s;
+	for (int i = 0; i < n; i++) {
+		cin >> tmp;
+		while (!s.empty() && s.top() <= tmp)
+			s.pop();
+		if (s.empty())
+			s.push(tmp);
+		else {
+			res += s.size();
+			s.push(tmp);
+		}
+	}
+	cout << res;
 }
