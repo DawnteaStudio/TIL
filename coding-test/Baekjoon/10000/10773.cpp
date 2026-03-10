066bf3a5123da2115763fc97344a9989fcd22402
GitHub Action <action@github.com>
2026-03-11 01:49:31 +0900

    10773 solve

--- coding-test/Baekjoon/10000/10773.cpp
+++ coding-test/Baekjoon/10000/10773.cpp
@@ -3,23 +3,24 @@
 #define fast ios::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
 using namespace std;
 
+int main()
+{
+	fast;
+	int n, tmp;
+	cin >> n;
 
-int main() {
-    fast;
-    int n, tmp, res = 0;
-    cin >> n;
-    stack<int> s;
-    for (int i = 0; i < n; i++)
-    {
-        cin >> tmp;
-        res += tmp;
-        if (tmp != 0)
-            s.push(tmp);
-        else
-        {
-            res -= s.top();
-            s.pop();
-        }
-    }
-    cout << res;
+	stack<int> s;
+	for (int i = 0; i < n; i++) {
+		cin >> tmp;
+		if (tmp == 0)
+			s.pop();
+		else
+			s.push(tmp);
+	}
+	int res = 0;
+	while (!s.empty()) {
+		res += s.top();
+		s.pop();
+	}
+	cout << res;
 }
