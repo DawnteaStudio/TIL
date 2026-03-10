c4a2d4543aa32502fcbd4639db1126cbf9483b16
GitHub Action <action@github.com>
2026-03-11 03:44:44 +0900

    3015 solve

--- coding-test/Baekjoon/3000/3015.cpp
+++ coding-test/Baekjoon/3000/3015.cpp
@@ -1,32 +1,36 @@
 #include <iostream>
 #include <stack>
-#include <utility>
 #define fast ios::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
 using namespace std;
 
-long long ans;
-
-int main() {
+int main()
+{
 	fast;
-	
-	int n;
+	int n, tmp;
+	long long res = 0;
 	cin >> n;
 
-	stack<pair<int, long long> > s;
+	stack<pair<int, int>> s;
 	for (int i = 0; i < n; i++) {
-		int tmp;
-		long long same = 0;
 		cin >> tmp;
-
-		while (!s.empty() && s.top().first <= tmp) {
-			ans += s.top().second;
-			if (s.top().first == tmp)
-				same = s.top().second;
+		while (!s.empty() && s.top().first < tmp) {
+			res += s.top().second;
 			s.pop();
 		}
-		if (!s.empty())
-			ans++;
-		s.push({tmp, same + 1});
+		if (s.empty())
+			s.push({tmp, 1});
+		else {
+			if (s.top().first == tmp) {
+				res += s.top().second;
+				s.top().second++;
+				if (s.size() > 1)
+					res++;
+			}
+			else {
+				res++;
+				s.push({tmp, 1});
+			}
+		}
 	}
-	cout << ans;
+	cout << res;
 }
